e9dbfa556641749a6692d1a5077896d1c9c867e3
chenhg5 <chg80333@163.com>
2024-05-15 09:25:15 +0800

    initial

--- internal/api/route.go
+++ internal/api/route.go
@@ -1,1 +1,19 @@
 package api
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoute(r *gin.Engine) {
+	api := r.Group("/api")
+
+	docAPI := api.Group("/doc")
+	docAPI.POST("")
+	docAPI.GET("")
+	docAPI.GET("/:docid/fragments")
+	docAPI.PUT("/:docid/fragment/:fragmentid")
+
+	botAPI := api.Group("/bot")
+	botAPI.POST("")
+	botAPI.PUT("")
+	botAPI.GET("/list")
+	botAPI.GET("")
+}
